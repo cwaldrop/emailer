88bf31fb03da7e7a1e48f56dec3b37a98d51e96a
Cabewaldrop <Cabewaldrop@yahoo.com>
2013-07-29 20:28:44 -0500

    --Proof of concept established.  First working email template created.

---
+++ emailer.py
@@ -0,0 +1,8 @@
+### Author: Cabe Waldrop
+### Email: cabetechrecruiter@gmail.com
+### The functions in this script can be used to create plain text for emails that I repeatedly send out
+
+def skype_request(candidate_name, title, contract_length, employer_name, employer_company):
+	print employee_name + """, \n \nI received your resume from """ + employer_name + " at " + employer_company + """ and thought your background was an excellent match to a """ + title + """ position we are recruiting for. This would initially be a """ + contract_length + """ contract with our client in Birmingham, AL, but the position is open for renewal.  If you are interested, I would like to set up a time where we could speak over Skype, perhaps 10-15 minutes to talk about the position and go over your experience in further detail. Please let me know when would be a good time for you.  Thanks and have a great day!"""
+
+#def interview_details()
